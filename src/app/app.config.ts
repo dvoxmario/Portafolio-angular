6612c9de127e3bb4b9b6b24bb14a7da3ee1fc52c
John Mario Chaverra Cespedes <154362790+dvoxmario@users.noreply.github.com>
2025-08-15 17:12:19 -0500

    se crea ci-cd

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -2,17 +2,17 @@ import { ApplicationConfig, importProvidersFrom, provideBrowserGlobalErrorListen
 import { provideRouter } from '@angular/router';
 
 import { routes } from './app.routes';
-import { RECAPTCHA_V3_SITE_KEY, RecaptchaV3Module } from 'ng-recaptcha';
+// import { RECAPTCHA_V3_SITE_KEY, RecaptchaV3Module } from 'ng-recaptcha';
 
 export const appConfig: ApplicationConfig = {
   providers: [
     provideBrowserGlobalErrorListeners(),
     provideZonelessChangeDetection(),
     provideRouter(routes),
-    importProvidersFrom(RecaptchaV3Module),
-    {
-      provide: RECAPTCHA_V3_SITE_KEY,
-      useValue: '6LeRH5wrAAAAAIWpIs77QLXbGIFYIY86l5mNCOjT' 
-    }
+    // importProvidersFrom(RecaptchaV3Module),
+    // {
+    //   provide: RECAPTCHA_V3_SITE_KEY,
+    //   useValue: '6LeRH5wrAAAAAIWpIs77QLXbGIFYIY86l5mNCOjT' 
+    // }
   ]
 };
